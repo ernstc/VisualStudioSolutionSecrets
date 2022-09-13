d5fbbca614bc131e5906e39b1fcd5d190c8ef0ca
Ernesto Cianciotta <ernesto.cianciotta@gmail.com>
2022-09-13 18:23:25 +0200

    Added project for unit tests.

---
+++ VisualStudioSolutionSecrets.Tests/Encryption/Cipher.Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualStudioSolutionSecrets.Tests.Encryption
+{
+    public class CipherTests
+    {
+        [Fact]
+        public void InitWithPassphrase()
+        {
+
+        }
+
+
+        [Fact]
+        public void InitWithFile()
+        {
+
+        }
+
+
+        [Fact]
+        public void EncryptionTest()
+        {
+
+        }
+
+
+        [Fact]
+        public void DecryptionTest()
+        {
+
+        }
+
+    }
+}
---
+++ VisualStudioSolutionSecrets.Tests/HeaderFile.Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualStudioSolutionSecrets.Tests
+{
+    public class HeaderFileTests
+    {
+
+        [Theory]
+        [InlineData("1.0")]
+        [InlineData("1.0.0")]
+        [InlineData("1.1.2")]
+        [InlineData("1.1.3")]
+        [InlineData("1.9.1")]
+        [InlineData("1.9.1.1")]
+
+        public void IsVersionSupportedTests(string version)
+        {
+            HeaderFile headerFile = new HeaderFile();
+            headerFile.visualStudioSolutionSecretsVersion = version;
+            bool isSupported = headerFile.IsVersionSupported();
+
+            Assert.True(isSupported);
+        }
+
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("1")]
+        [InlineData("2.0.0")]
+        [InlineData("1.9.1.1.1")]
+        public void IsVersionNotSupportedTests(string version)
+        {
+            HeaderFile headerFile = new HeaderFile();
+            headerFile.visualStudioSolutionSecretsVersion = version;
+            bool isSupported = headerFile.IsVersionSupported();
+
+            Assert.False(isSupported);
+        }
+    }
+}
