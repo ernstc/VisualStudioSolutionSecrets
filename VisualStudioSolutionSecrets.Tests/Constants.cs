17117d4cbd8b4563ff33fe7126f85992d3d22587
Ernesto Cianciotta <ernesto.cianciotta@gmail.com>
2022-09-14 18:23:05 +0200

    Added sample files for unit testing.

---
+++ VisualStudioSolutionSecrets.Tests/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualStudioSolutionSecrets.Tests
+{
+    public static class Constants
+    {
+        public static readonly string SampleFilesPath = Helpers.GetAbsoluteTestPath("SampleFiles");
+        public static readonly string ConfigFilesPath = Path.Combine(SampleFilesPath, "configFiles");
+        public static readonly string SecretFilesPath = Path.Combine(SampleFilesPath, "secrets");
+        public static readonly string SolutionFilesPath = Path.Combine(SampleFilesPath, "solutionFiles");
+    }
+}
---
+++ VisualStudioSolutionSecrets.Tests/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualStudioSolutionSecrets.Tests
+{
+    public static class Helpers
+    {
+        public static string GetAbsoluteTestPath(string relativePath)
+        {
+            var codeBaseUrl = new Uri(Assembly.GetExecutingAssembly().CodeBase);
+            var codeBasePath = Uri.UnescapeDataString(codeBaseUrl.AbsolutePath);
+            var dirPath = Path.GetDirectoryName(codeBasePath);
+            return Path.Combine(dirPath, relativePath);
+        }
+    }
+}
