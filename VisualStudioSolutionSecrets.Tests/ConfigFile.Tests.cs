491d9bccc69993c6ae7ce56b482320bdaa377340
Ernesto Cianciotta <ernesto.cianciotta@gmail.com>
2022-09-14 18:26:39 +0200

    Added tests.

--- VisualStudioSolutionSecrets.Tests/ConfigFile.Tests.cs
+++ VisualStudioSolutionSecrets.Tests/ConfigFile.Tests.cs
@@ -3,10 +3,107 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+using VisualStudioSolutionSecrets.Encryption;
+using VisualStudioSolutionSecrets.IO;
+using VisualStudioSolutionSecrets.Repository;
 
 namespace VisualStudioSolutionSecrets.Tests
 {
-    internal class ConfigFile
+    public class ConfigFileTests
     {
+
+        [Fact]
+        public void ConfigFileEncryption()
+        {
+            var cipher = new Mock<ICipher>();
+            var repository = new Mock<IRepository>();
+
+            cipher.Setup(o => o.Encrypt(It.IsAny<string>())).Returns("encrypted");
+
+            Context.Create(
+                new DefaultFileSystem(),
+                cipher.Object,
+                repository.Object
+                );
+
+            string configFilePath = Path.Combine(Constants.ConfigFilesPath, "configFile.json");
+
+            ConfigFile configFile = new ConfigFile(configFilePath, "uniqueConfigFile.json", Context.Current.Cipher);
+            bool succes = configFile.Encrypt();
+            Assert.True(succes);
+            Assert.Equal("encrypted", configFile.Content);
+        }
+
+
+        [Fact]
+        public void ConfigFileEncryptionFailed()
+        {
+            var cipher = new Mock<ICipher>();
+            var repository = new Mock<IRepository>();
+
+            cipher.Setup(o => o.Encrypt(It.IsAny<string>())).Returns((string)null);
+
+            Context.Create(
+                new DefaultFileSystem(),
+                cipher.Object,
+                repository.Object
+                );
+
+            string configFilePath = Path.Combine(Constants.ConfigFilesPath, "configFile.json");
+
+            ConfigFile configFile = new ConfigFile(configFilePath, "uniqueConfigFile.json", Context.Current.Cipher);
+            string? content = configFile.Content;
+            bool success = configFile.Encrypt();
+            Assert.False(success);
+            Assert.Equal(content, configFile.Content);
+        }
+
+
+        [Fact]
+        public void ConfigFileDecryption()
+        {
+            var cipher = new Mock<ICipher>();
+            var repository = new Mock<IRepository>();
+
+            cipher.Setup(o => o.Decrypt(It.IsAny<string>())).Returns("decrypted");
+
+            Context.Create(
+                new DefaultFileSystem(),
+                cipher.Object,
+                repository.Object
+                );
+
+            string configFilePath = Path.Combine(Constants.ConfigFilesPath, "configFile.json");
+
+            ConfigFile configFile = new ConfigFile(configFilePath, "uniqueConfigFile.json", Context.Current.Cipher);
+            bool success = configFile.Decrypt();
+            Assert.True(success);
+            Assert.Equal("decrypted", configFile.Content);
+        }
+
+
+        [Fact]
+        public void ConfigFileDecryptionFailed()
+        {
+            var cipher = new Mock<ICipher>();
+            var repository = new Mock<IRepository>();
+
+            cipher.Setup(o => o.Encrypt(It.IsAny<string>())).Returns((string)null);
+
+            Context.Create(
+                new DefaultFileSystem(),
+                cipher.Object,
+                repository.Object
+                );
+
+            string configFilePath = Path.Combine(Constants.ConfigFilesPath, "configFile.json");
+
+            ConfigFile configFile = new ConfigFile(configFilePath, "uniqueConfigFile.json", Context.Current.Cipher);
+            string? content = configFile.Content;
+            bool success = configFile.Decrypt();
+            Assert.False(success);
+            Assert.Equal(content, configFile.Content);
+        }
     }
 }
--- VisualStudioSolutionSecrets.Tests/SolutionFile.Tests.cs
+++ VisualStudioSolutionSecrets.Tests/SolutionFile.Tests.cs
@@ -3,10 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+using VisualStudioSolutionSecrets.Encryption;
+using VisualStudioSolutionSecrets.IO;
+using VisualStudioSolutionSecrets.Repository;
 
 namespace VisualStudioSolutionSecrets.Tests
 {
-    internal class SolutionFile
+    public class SolutionFileTests
     {
+
+        private IFileSystem _fileSystem;
+
+        public SolutionFileTests()
+        {
+            var defaultFileSystem = new DefaultFileSystem();
+            var fileSystemMock = new Mock<IFileSystem>();
+
+            fileSystemMock
+                .Setup(o => o.GetFileInfo(It.IsAny<string>()))
+                .Returns((string path) => defaultFileSystem.GetFileInfo(path));
+
+            fileSystemMock
+                .Setup(o => o.FileReadAllLines(It.IsAny<string>()))
+                .Returns((string path) => defaultFileSystem.FileReadAllLines(path));
+
+            fileSystemMock
+                .Setup(o => o.GetApplicationDataFolderPath())
+                .Returns(Constants.SampleFilesPath);
+
+            fileSystemMock
+                .Setup(o => o.GetUserProfileFolderPath())
+                .Returns(Constants.SampleFilesPath);
+
+            _fileSystem = fileSystemMock.Object;
+        }
+
+
+        [Fact]
+        public void GetProjectsSecretConfigFiles()
+        {
+            Context.Create(
+                _fileSystem,
+                new Mock<ICipher>().Object,
+                new Mock<IRepository>().Object
+                );
+
+            var solutionFilePath = Path.Combine(Constants.SolutionFilesPath, "SolutionSample.sln");
+            SolutionFile solutionFile = new SolutionFile(solutionFilePath);
+
+            var secretConfigFiles = solutionFile.GetProjectsSecretConfigFiles();
+
+            Assert.NotNull(secretConfigFiles);
+        }
+
+
+        [Fact]
+        public void SaveConfigFile()
+        {
+            Assert.Fail("Not implemented.");
+        }
+
     }
 }
