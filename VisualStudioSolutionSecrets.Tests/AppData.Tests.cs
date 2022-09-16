89a3c9e1b009a2404f0220b299e1a8e70475b086
Ernesto Cianciotta <ernesto.cianciotta@gmail.com>
2022-09-16 18:12:03 +0200

    Added tests for Context and AppData management.

---
+++ VisualStudioSolutionSecrets.Tests/AppData.Tests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using VisualStudioSolutionSecrets.IO;
+
+namespace VisualStudioSolutionSecrets.Tests
+{
+    public class AppDataTests
+    {
+
+        const string FILE_NAME = "testAppData.json";
+        const string REFERENCE_FILE_NAME = "configFile.json";
+
+
+        private class SampleData
+        {
+            public int IntValue { get; set; }
+            public string StringValue { get; set; } = null!;
+            public DateTime DateTimeValue { get; set; }
+            public DateTimeOffset DateTimeOffsetValue { get; set; }
+            public bool BoolValue { get; set; }
+            public int[] IntArrayValue { get; set; } = null!;
+            public InnerSampleData ObjectValue { get; set; } = null!;
+        }
+
+
+        private class InnerSampleData
+        {
+            public int IntValue { get; set; }
+            public string? StringValue { get; set; }
+        }
+
+
+        private static SampleData GetSampleData()
+        {
+            return new SampleData
+            {
+                IntValue = 1,
+                StringValue = "string",
+                DateTimeValue = new DateTime(2022, 09, 16, 16, 00, 01),
+                DateTimeOffsetValue = new DateTimeOffset(2022, 09, 16, 16, 00, 01, TimeSpan.FromHours(2)),
+                BoolValue = true,
+                IntArrayValue = new int[] { 100, 200 },
+                ObjectValue = new InnerSampleData
+                {
+                    IntValue = 2,
+                    StringValue = null
+                }
+            };
+        }
+
+
+        private static void ConfigureContext()
+        {
+            var fileSystemMock = new Mock<DefaultFileSystem>();
+
+            fileSystemMock
+                .Setup(o => o.GetApplicationDataFolderPath())
+                .Returns(Constants.ConfigFilesPath);
+
+            Context.Configure(context =>
+            {
+                context.IO = fileSystemMock.Object;
+            });
+        }
+
+
+        [Fact]
+        public void SaveDataTest()
+        {
+            ConfigureContext();
+
+            SampleData data = GetSampleData();
+
+            AppData.SaveData(FILE_NAME, data);
+
+            string filePath = Helpers.GetAbsoluteTestPath(Path.Combine(Constants.ConfigFilesPath, FILE_NAME));
+
+            Assert.True(File.Exists(filePath));
+
+            string referenceFilePath = Helpers.GetAbsoluteTestPath(Path.Combine(Constants.ConfigFilesPath, REFERENCE_FILE_NAME));
+            string referenceContent = File.ReadAllText(referenceFilePath);
+            string content = File.ReadAllText(filePath);
+
+            Assert.Equal(referenceContent, content);
+        }
+
+
+        [Fact]
+        public void LoadDataTest()
+        {
+            ConfigureContext();
+           
+            SampleData data = GetSampleData();
+
+            var loadedData = AppData.LoadData<SampleData>(REFERENCE_FILE_NAME);
+
+            Assert.Equivalent(data, loadedData, strict: true);
+        }
+    }
+}
---
+++ VisualStudioSolutionSecrets.Tests/Context.Tests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using VisualStudioSolutionSecrets.Encryption;
+using VisualStudioSolutionSecrets.IO;
+using VisualStudioSolutionSecrets.Repository;
+
+namespace VisualStudioSolutionSecrets.Tests
+{
+    public class ContextTests
+    {
+        [Fact]
+        public void DefaultContextTest()
+        {
+            Assert.NotNull(Context.Current);
+        }
+
+
+        [Fact]
+        public void SetIODependencyTest()
+        {
+            // Test the dependency assignment
+            var dependency = new Mock<IFileSystem>().Object;
+            Context.Configure(context => context.IO = dependency);
+            Assert.Equal(dependency, Context.Current.IO);
+
+            // Check that null assignment does not change the dependency
+            Context.Configure(context => context.IO = null);
+            Assert.Equal(dependency, Context.Current.IO);
+        }
+
+
+        [Fact]
+        public void SetCipherDependencyTest()
+        {
+            // Test the dependency assignment
+            var dependency = new Mock<ICipher>().Object;
+            Context.Configure(context => context.Cipher = dependency);
+            Assert.Equal(dependency, Context.Current.Cipher);
+
+            // Check that null assignment does not change the dependency
+            Context.Configure(context => context.Cipher = null);
+            Assert.Equal(dependency, Context.Current.Cipher);
+        }
+
+
+        [Fact]
+        public void SetRepositoryDependencyTest()
+        {
+            // Test the dependency assignment
+            var dependency = new Mock<IRepository>().Object;
+            Context.Configure(context => context.Repository = dependency);
+            Assert.Equal(dependency, Context.Current.Repository);
+
+            // Check that null assignment does not change the dependency
+            Context.Configure(context => context.Repository = null);
+            Assert.Equal(dependency, Context.Current.Repository);
+        }
+
+
+        [Fact]
+        public void VersionStringTest()
+        {
+            Assert.False(String.IsNullOrWhiteSpace(Context.Current.VersionString));
+        }
+
+
+        [Fact]
+        public void CurrentVersionTest()
+        {
+            Assert.NotNull(Context.Current.CurrentVersion);
+        }
+    }
+}
