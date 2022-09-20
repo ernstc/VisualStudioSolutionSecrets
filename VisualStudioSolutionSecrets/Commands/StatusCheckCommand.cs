b9b928ea2f020c63ba1e4881090fa7c07981cb98
Ernesto Cianciotta <ernesto.cianciotta@gmail.com>
2022-09-20 18:33:39 +0200

    Improved solutions status output.

--- VisualStudioSolutionSecrets/Commands/StatusCheckCommand.cs
+++ VisualStudioSolutionSecrets/Commands/StatusCheckCommand.cs
@@ -28,12 +28,12 @@ namespace VisualStudioSolutionSecrets.Commands
 
             if (isCipherReady && isRepositoryReady && options.Path != null)
             {
-                Console.WriteLine("Checking solutions status...\n\n");
+                Console.WriteLine("Checking solutions synchronization status...\n");
 
                 string? path = EnsureFullyQualifiedPath(options.Path);
 
-                Console.WriteLine("Solution                                |  Version                 |  Last Update          |  Status");
-                Console.WriteLine("------------------------------------------------------------------------------------------------------");
+                Console.WriteLine("Solution                                          |  Version   |  Last Update          |  Status");
+                Console.WriteLine("------------------------------------------------------------------------------------------------------------");
 
                 string[] solutionFiles = GetSolutionFiles(path, options.All);
                 foreach (string solutionFile in solutionFiles)
@@ -46,67 +46,113 @@ namespace VisualStudioSolutionSecrets.Commands
 
         private async Task GetSolutionStatus(string solutionFile)
         {
-            string version;
-            string lastUpdate;
+            string version = String.Empty;
+            string lastUpdate = String.Empty;
             string status;
 
+            ConsoleColor statusColor;
+            ConsoleColor solutionColor;
+
             SolutionFile solution = new SolutionFile(solutionFile, Context.Cipher);
-
-            Context.Repository.SolutionName = solution.Name;
-
-            var repositoryFiles = await Context.Repository.PullFilesAsync();
-            if (repositoryFiles.Count == 0)
+            var configFiles = solution.GetProjectsSecretConfigFiles();
+            if (configFiles.Count == 0)
             {
-                version = String.Empty;
-                lastUpdate = String.Empty;
-                status = "ERROR";
+                // This solution has not projects with secrets.
+                status = "No secrests found";
+                statusColor = ConsoleColor.DarkGray;
+                solutionColor = ConsoleColor.DarkGray;
             }
             else
             {
-                status = "OK";
-                HeaderFile? header = null;
-               
-                foreach (var file in repositoryFiles)
+                solutionColor = ConsoleColor.White;
+
+                Context.Repository.SolutionName = solution.Name;
+
+                var repositoryFiles = await Context.Repository.PullFilesAsync();
+                if (repositoryFiles.Count == 0)
                 {
-                    if ((
-                        file.name == "secrets.json"
-                        || file.name == "secrets"   // This check is for compatibility with versions <= 1.1.x
-                        )
-                        && file.content != null)
-                    {
-                        header = JsonSerializer.Deserialize<HeaderFile>(file.content);
-                        continue;
-                    }
+                    status = "Local only";
+                    statusColor = ConsoleColor.Gray;
+                }
+                else
+                {
+                    status = "OK";
+                    statusColor = ConsoleColor.Cyan;
 
-                    if (file.content == null)
-                    {
-                        status = "ERROR";
-                        break;
-                    }
+                    HeaderFile? header = null;
 
-                    bool isFileOk = true;
-                    var contents = JsonSerializer.Deserialize<Dictionary<string, string>>(file.content);
-                    foreach (var item in contents)
+                    foreach (var file in repositoryFiles)
                     {
-                        string? decryptedContent = Context.Current.Cipher.Decrypt(item.Value);
-                        if (decryptedContent == null)
+                        if ((
+                            file.name == "secrets.json"
+                            || file.name == "secrets"   // This check is for compatibility with versions <= 1.1.x
+                            )
+                            && file.content != null)
                         {
-                            isFileOk = false;
+                            header = JsonSerializer.Deserialize<HeaderFile>(file.content);
+                            continue;
+                        }
+
+                        if (file.content == null)
+                        {
+                            status = "ERROR";
+                            statusColor = ConsoleColor.Red;
+                            break;
+                        }
+
+                        bool isFileOk = true;
+                        var contents = JsonSerializer.Deserialize<Dictionary<string, string>>(file.content);
+                        foreach (var item in contents)
+                        {
+                            string? decryptedContent = Context.Current.Cipher.Decrypt(item.Value);
+                            if (decryptedContent == null)
+                            {
+                                isFileOk = false;
+                                break;
+                            }
+                        }
+
+                        if (!isFileOk)
+                        {
+                            status = "INVALID KEY!";
+                            statusColor = ConsoleColor.Red;
+
                             break;
                         }
                     }
 
-                    if (!isFileOk)
-                    {
-                        status = "ERROR";
-                        break;
-                    }
+                    version = header?.visualStudioSolutionSecretsVersion ?? String.Empty;
+                    lastUpdate = header?.lastUpload.ToString("yyyy-MM-dd HH:mm:ss") ?? String.Empty;
                 }
-
-                version = header?.visualStudioSolutionSecretsVersion ?? String.Empty;
-                lastUpdate = header?.lastUpload.ToString("yyyy-MM-dd HH:mm:ss") ?? String.Empty;
             }
-            Console.WriteLine($"{solution.Name,-38}  |  {version,-22}  |  {lastUpdate,-19}  |  {status}");
+
+            string solutionName = solution.Name;
+            if (solutionName.Length > 48) solutionName = solutionName.Substring(0, 45) + "...";
+
+            var color = Console.ForegroundColor;
+
+            Console.ForegroundColor = solutionColor;
+            Console.Write($"{solutionName,-48}");
+
+            Console.ForegroundColor = color;
+            Console.Write("  |  ");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write($"{version,-8}");
+
+            Console.ForegroundColor = color;
+            Console.Write("  |  ");
+
+            Console.ForegroundColor = color;
+            Console.Write($"{lastUpdate,-19}");
+
+            Console.ForegroundColor = color;
+            Console.Write("  |  ");
+
+            Console.ForegroundColor = statusColor;
+            Console.WriteLine($"{status}");
+
+            Console.ForegroundColor = color;
         }
 
     }
