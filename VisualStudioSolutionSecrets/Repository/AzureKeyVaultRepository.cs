b04717f8d2123e667249fc40f9fe72edb01325d8
Ernesto Cianciotta <ernesto.cianciotta@gmail.com>
2022-10-03 21:38:33 +0200

    Improved Azure authentication for enabling token caching.

--- VisualStudioSolutionSecrets/Repository/AzureKeyVaultRepository.cs
+++ VisualStudioSolutionSecrets/Repository/AzureKeyVaultRepository.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Azure.Core;
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 
@@ -11,22 +15,117 @@ namespace VisualStudioSolutionSecrets.Repository
     public class AzureKeyVaultRepository : IRepository
     {
 
+        private readonly IDictionary<string, string> _clouds = new Dictionary<string, string>
+        {
+            {"AzureCloud",         ".vault.azure.net" },
+            {"AzureChinaCloud",    ".vault.azure.cn" },
+            {"AzureUSGovernment",  ".vault.usgovcloudapi.net" },
+            {"AzureGermanCloud",   ".vault.microsoftazure.de" }
+        };
+
+        private const string DEFAULT_CLOUD = "AzureCloud";
         private const string SECRET_PREFIX = "vs-secrets--";
 
         public bool EncryptOnClient => false;
         public string RepositoryType => "AzureKV";
-        public string? RepositoryName { get; set; }
+        
+        private string? _repositoryName;
+
+        public string? RepositoryName
+        {
+            get => _repositoryName;
+            set
+            {
+                if (value == null)
+                {
+                    _repositoryName = null;
+                }
+                else
+                {
+                    string loweredValue = value.ToLower();
+                    if (Uri.TryCreate(loweredValue, UriKind.Absolute, out Uri? repositoryUri) && repositoryUri != null)
+                    {
+                        string cloudDomain = loweredValue[loweredValue.IndexOf(".vault.")..];
+                        if (_clouds.Any(item => item.Value == cloudDomain))
+                        {
+                            _repositoryName = repositoryUri.Scheme.Equals("https", StringComparison.OrdinalIgnoreCase) ? loweredValue : null;
+                        }
+                        else
+                        {
+                            _repositoryName = null;
+                        }
+                    }
+                    else
+                    {
+                        _repositoryName = $"https://{loweredValue}{_clouds[DEFAULT_CLOUD]}";
+                    }
+                }
+            }
+        }
 
 
         private SecretClient? _client;
 
 
-        public Task AuthorizeAsync()
+
+        public string? GetFriendlyName()
         {
-            var kvUri = "https://" + RepositoryName + ".vault.azure.net";
-            var credential = new ChainedTokenCredential(new DefaultAzureCredential(), new InteractiveBrowserCredential());
-            _client = new SecretClient(new Uri(kvUri), credential);
-            return Task.CompletedTask;
+            if (RepositoryName == null)
+            {
+                return null;
+            }
+
+            string name = RepositoryName;
+            name = name[8..];
+            name = name[..name.IndexOf(".vault.")];
+
+            string cloudDomain = RepositoryName;
+            cloudDomain = cloudDomain[cloudDomain.IndexOf(".vault.")..];
+
+            var cloud = _clouds.First(entry => entry.Value.Equals(cloudDomain, StringComparison.OrdinalIgnoreCase));
+
+            return $"{cloud.Key} ({name})";
+        }
+
+
+        public async Task AuthorizeAsync()
+        {
+            if (_repositoryName != null)
+            {
+                var tokenCachePersistenceOptions = new TokenCachePersistenceOptions
+                {
+                    Name = "vs-secrets",
+                    UnsafeAllowUnencryptedStorage = false
+                };
+
+                var interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions();
+                interactiveBrowserCredentialOptions.TokenCachePersistenceOptions = tokenCachePersistenceOptions;
+                interactiveBrowserCredentialOptions.AdditionallyAllowedTenants.Add("*");
+
+                async Task AuthorizeClientAsync()
+                {
+                    var credential = new ChainedTokenCredential(
+                        new SharedTokenCacheCredential(new SharedTokenCacheCredentialOptions(tokenCachePersistenceOptions)),
+                        new InteractiveBrowserCredential(interactiveBrowserCredentialOptions)
+                    );
+
+                    var accessToken = await credential.GetTokenAsync(new TokenRequestContext(/*scopes: new string[] { "https://vault.azure.net/.default" }*/));
+
+                    _client = new SecretClient(new Uri(_repositoryName!), credential);
+                }
+
+                await AuthorizeClientAsync();
+                try
+                {
+                    var _ = await _client!.GetSecretAsync("vs-secrets-fake");
+                }
+                catch (Azure.Identity.AuthenticationFailedException)
+                {
+                    await AuthorizeClientAsync();
+                }
+                catch
+                { }
+            }
         }
 
 
@@ -105,7 +204,7 @@ namespace VisualStudioSolutionSecrets.Repository
                 {
                     fileName = fileName.Substring(fileName.IndexOf('\\') + 1);
                     fileName = fileName.Substring(0, fileName.IndexOf('.'));
-                }    
+                }
                 string secretName = $"{SECRET_PREFIX}{solutionName}--{fileName}";
                 try
                 {
